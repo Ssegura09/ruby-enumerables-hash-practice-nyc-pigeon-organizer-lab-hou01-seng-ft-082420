933cc8e7f143902f1f1be8951c4f439d3c86887f
Stephanie M Segura <stephaniemsegura09@gmail.com>
2020-08-09 21:06:20 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,6 @@
 require 'pry'
 def nyc_pigeon_organizer(data)
-  data.each_with_object({}) do |(key, value), a|
- a[k] = v + 3
+  data.each_with_object({}) do |(key, value), final_array|
+ final_array[key] = v + 3
   binding.pry
 end
